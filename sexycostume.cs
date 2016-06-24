d872b9cf0de177531cf30104cfdd95d9a741f485
Mystasly <mystasly48@gmail.com>
2016-06-24 22:42:19 +0900

    sexy衣装
    前進－後退を計算し、０未満の場合には０と出力し、０以上の場合にはその数値を出力する。

---
+++ sexycostume.cs
@@ -0,0 +1,12 @@
+using System;
+public class Hello {
+  public static void Main() {
+    var line = Console.ReadLine();
+    var array = line.Split(' ');
+    var forward = int.Parse(array[0]);
+    var backward = int.Parse(array[1]);
+    var result = forward - backward;
+    } else {
+    }
+  }
+}
