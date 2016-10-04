fab3d0da225bef41b6cce8f83d3564ec32260b7f
Maisu <mystasly48@gmail.com>
2016-10-04 21:07:32 +0900

    水着（最後の判定で失敗）
    再挑戦しよう！ということで LINQ+Hashtable+foreach
    でやってみましたが１０万文字の判定で時間切れになりました。LINQ+Hashtableだけでならどうにかできたのでしょうが、連続foreachで処理時間が伸びてしまったようです。この問題、C#で解けるのかな・・・・・。

---
+++ error2_mizugi.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using System.Collections;
+
+public class Program {
+  public static void Main() {
+    var _____ = Console.ReadLine();
+    var beforeArray = Console.ReadLine().ToArray();
+    var afterArray = Console.ReadLine().ToArray();
+    var beforeHash = new Hashtable();
+    var afterHash = new Hashtable();
+    var resultHash = new Hashtable();
+    var result = 0;
+
+    foreach (var c in beforeArray)
+      beforeHash[c] = beforeArray.Count(_ => _ == c);
+
+    foreach (var c in afterArray)
+      afterHash[c] = afterArray.Count(_ => _ == c);
+
+    foreach (var key in afterHash.Keys)
+      if (beforeHash.ContainsKey(key))
+        resultHash[key] = afterHash[key].GetHashCode() - beforeHash[key].GetHashCode();
+      else
+        result += (int)afterHash[key];
+
+    foreach (var key in resultHash.Keys)
+      if ((int)resultHash[key] > 0)
+        result += (int)resultHash[key];
+
+    Console.WriteLine(result);
+  }
+}
