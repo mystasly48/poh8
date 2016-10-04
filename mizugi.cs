be1f81d99fdabe7b5301678d5823015d46e3c285
Maisu <mystasly48@gmail.com>
2016-10-04 21:19:14 +0900

    水着（成功！）
    無駄に重複した処理をループさせていたのが負担になっていたようなので、Distinct() で重複を排除してループさせたら解決した。１０万文字でも
    0.11秒 でクリアできた。

--- mizugi.cs
+++ mizugi.cs
@@ -1,8 +1,33 @@
 using System;
-public class Hello {
-  public static void Main() {
-    var line = Console.ReadLine();
+using System.Linq;
+using System.Collections;
 
-    Console.WriteLine();
+public class Program {
+  public static void Main() {
+    var _____ = Console.ReadLine();
+    var beforeArray = Console.ReadLine().ToArray();
+    var afterArray = Console.ReadLine().ToArray();
+    var beforeHash = new Hashtable();
+    var afterHash = new Hashtable();
+    var resultHash = new Hashtable();
+    var result = 0;
+
+    foreach (var c in beforeArray.Distinct())
+      beforeHash[c] = beforeArray.Count(_ => _ == c);
+  
+    foreach (var c in afterArray.Distinct())
+      afterHash[c] = afterArray.Count(_ => _ == c);
+
+    foreach (var key in afterHash.Keys)
+      if (beforeHash.ContainsKey(key))
+        resultHash[key] = afterHash[key].GetHashCode() - beforeHash[key].GetHashCode();
+      else
+        result += (int)afterHash[key];
+
+    foreach (var key in resultHash.Keys)
+      if ((int)resultHash[key] > 0)
+        result += (int)resultHash[key];
+
+    Console.WriteLine(result);
   }
 }
