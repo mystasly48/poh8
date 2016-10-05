3e1d68343a4f73a5b65f94d94773e2ebb67ea57a
Maisu <mystasly48@gmail.com>
2016-10-05 21:20:47 +0900

    制服を編集（作業中）

--- uiform.cs
+++ uiform.cs
@@ -1,7 +1,31 @@
-using System;
+﻿using System;
 
 public class Hello {
+  var WeakToStrong = new string[] { "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2" };
+  var CardsCount = 52;
   public static void Main() {
-    
+    var cards = Console.ReadLine().Split(' ');
+    var cardsRank = new int[CardsCount];
+    var currentRank = 0;
+    var currentCard = "";
+
+    cardsRank[0] = currentRank;
+    currentCard = cards[0];
+
+    while (currentRank != 52) {
+      if (currentCard == "2") currentCard = "";
+      if (currentCard == "") {
+        var index = GetIndex(cards, 
+        
+
+      for (int i = 0; i < CardsCount; i++) {
+        
+      }
+    }
+  }
+  
+  public static int GetIndex(string[] array, string word) {
+    var index = Array.IndexOf(array, word);
+    return index;
   }
 }
