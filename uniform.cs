5ada641e238895f3576c504b94c3a0bad3509821
Maisu <mystasly48@gmail.com>
2017-02-25 00:24:22 +0900

    uiform to uniform

--- uniform.cs
+++ uniform.cs
@@ -1,24 +1,33 @@
 ﻿using System;
 using System.Linq;
 
-public class Hello {
+public class Program {
   public static void Main() {
     string[] WeakToStrong = { "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2" };
     string[] cards = Console.ReadLine().Split(' ');
     int[] ranks = new int[cards.Length];
+    int cardIndex = 0;
     int currentRankIndex = 0;
     string currentCard = "2";
     int currentRank = 1;
-    for (int cardIndex = 0, currentRankIndex = 0, currentRank = 1, currentCard = "2"; currentRank < cards.Length; cardIndex) {
-      if (currentCard == "2") {
+    while (currentRank < cards.Length) {
+      if (currentCard == "2" || Array.IndexOf(WeakToStrong, currentCard) < Array.IndexOf(WeakToStrong, cards[cardIndex])) {
         ranks[currentRankIndex] = currentRank;
         currentRank++;
         currentRankIndex++;
         currentCard = cards[cardIndex];
-        cards[i] = "";
-      } eles {
-
+        cards[cardIndex] = "";
       }
+
+      if (cardIndex >= cards.Length) {
+        cardIndex = 0;
+      } else {
+        cardIndex++;
+      }
+    }
+
+    foreach (var i in ranks) {
+      Console.WriteLine(i);
     }
   }
 }
